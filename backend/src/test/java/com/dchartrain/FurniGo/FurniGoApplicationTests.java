334342fdbfc3ad9d3d36701dd1d7ada7292387f5
donovan-dev-web <donovan.dev3d@gmail.com>
2026-04-07 12:24:07 +0200

    chore: Initialiser le projet Spring Boot

---
+++ backend/src/test/java/com/dchartrain/FurniGo/FurniGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dchartrain.FurniGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurniGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
